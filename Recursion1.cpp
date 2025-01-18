308229488bf54f202ef0e668f289bc0ac5887d14
Alphabtt <ragib834@gmail.com>
2025-01-18 23:48:48 +0600

    Create Recursion1.cpp
    Factorial with recursion

---
+++ Recursion1.cpp
@@ -0,0 +1,26 @@
+//factorial with recursion
+#include<iostream> 
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+        int smallerProblem = factorial(n-1);
+        int biggerProblem = n * smallerProblem;
+
+        return biggerProblem;
+    }
+
+int main()
+{
+int n;
+cin>>n;
+
+cout<<factorial(n);
+
+return 0;
+}
+
